5c78fa23d745802a8a17b5e45542c0350391482b
smgKV878 <kliment.antonov@gmail.com>
2024-05-28 11:18:52 +0300

    Initial commit

---
+++ src/com/company/Singleton.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Singleton {
+    private static Singleton instance;
+    private int counter=0;
+
+    private Singleton()
+    {
+        counter = 0;
+        System.out.println("Singleton is instantiated. ");
+    }
+
+    public static Singleton getInstance()
+    {
+        if(instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+    public void doSomething()
+    {
+        counter++;
+        System.out.println("Something is done. " + counter);
+    }
+}
